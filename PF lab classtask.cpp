c4249460a77ae1ca8d52f1de6fce5095f745dd8b
Maham874 <mahammustafa2007@gmail.com>
2025-09-15 10:32:07 +0500

    PF lab classtask

---
+++ PF lab classtask.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+int main (){
+	
+char qualification[10];
+char result[10];
+printf("Enter Your Qualification: ");
+scanf("%s" ,qualification);
+
+if(strcmp(qualification, "MS")==0)
+{
+	printf("Enter your Result: ");
+	scanf("%s", result);
+	if (strcmp(result,"Pass")==0)
+	{
+		printf("Congratulations You are hired");
+	}
+	else
+	{
+		printf("Better luck next time");
+	}
+}
+else 
+{
+	printf("not eligible");
+}
+}
